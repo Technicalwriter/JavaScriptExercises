41adf6db951b9ff46932b0d4118d5cea788e4216
Technicalwriter <Mr.Technical.Writer@gmail.com>
2013-07-23 23:41:55 -0400

    Update section1funtion1.js

--- ynonperek/section1funtion1.js
+++ ynonperek/section1funtion1.js
@@ -4,3 +4,15 @@
 
 x = Math.random() * (100 - 0)
   
+/**
+ * Showing work
+ * First, I see an answer in basic. Then, I translate it into javascript. Then, I apply the DRY philosophy
+ * random math => x
+ * random math => y
+ * random math => z
+ * 
+ * if x>y than x=>y
+ * if y>z than y=>z
+ * print z
+ * 
+ */
