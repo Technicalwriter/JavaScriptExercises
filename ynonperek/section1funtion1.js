c4d1d00e5dfc2c1a2da60e9e978ab87c07a7e193
Technicalwriter <Mr.Technical.Writer@gmail.com>
2013-07-24 13:09:00 -0400

    Update section1funtion1.js

--- ynonperek/section1funtion1.js
+++ ynonperek/section1funtion1.js
@@ -1,18 +1,41 @@
 /**
  * Randomizes three numbers in range (0.. 100) and prints the largest one.
  */
+<html>
+<head>
+</head>
+<body>
 
-x = Math.random() * (100 - 0)
-  
+<script type="text/javascript">
+for (x=0,x<3;x++) {
+  Math,random() * (100 - 0) => r
+  if r>R 
+  break;
+}
+
+document.write(r);
+
+</script>
+</body>
+</html>
+
+
 /**
  * Showing work
  * First, I see an answer in basic. Then, I translate it into javascript. Then, I apply the DRY philosophy
- * random math => x
- * random math => y
- * random math => z
+ * math.random => x
+ * math.random => y
+ * math.random => z
  * 
  * if x>y than x=>y
  * if y>z than y=>z
  * print z
  * 
+ * var x
+ * var y
+ * var z
+ * 
+ * x = Math.random() * (100 - 0)
+ * 
+ * document.write(z);
  */
